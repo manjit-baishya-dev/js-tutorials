441ff06a118e30b2ed1798329aabd008a3590b55
Manjit Baishya <manjitbaishya15@gmail.com>
2024-02-09 07:41:44 +0530

    new commit

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,12 @@
+const accId = 144559
+let accEmail = "manjitbaishya1@gmail.com"
+var accPass = "berHvdD8*09"
+accCity = "Jaipur"
+
+// accId = 548425  -->> NOT ALLOWED
+
+accEmail = "manjitbaishya15@gmail"
+accPass = "ajsrnHBO*780dev"
+accCity = "Guwahati"
+
+console.table([accEmail, accId, accPass, accCity])
